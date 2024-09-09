db12e6a7d5e96e77726f7f4d4579d743212f1526
Aleksey Yesin <esinru@gmail.com>
2024-09-09 14:31:53 +0300

    Add strength meter logic

--- src/app/common/password-strength-meter/password-strength-meter.component.ts
+++ src/app/common/password-strength-meter/password-strength-meter.component.ts
@@ -3,12 +3,9 @@ import { Component, Input } from '@angular/core';
 type SectionColor = 'gray' | 'red' | 'yellow' | 'green';
 type ColorClassMap = Record<SectionColor, string>;
 
-const colorClassMap: ColorClassMap = {
-  gray: 'strength-section--gray',
-  red: 'strength-section--red',
-  yellow: 'strength-section--yellow',
-  green: 'strength-section--green',
-};
+const lettersRegex = /[a-zA-Z]/;
+const digitsRegex = /[0-9]/;
+const specialCharsRegex = /[^a-zA-Z0-9]/;
 
 @Component({
   selector: 'app-password-strength-meter',
@@ -20,7 +17,33 @@ const colorClassMap: ColorClassMap = {
 export class PasswordStrengthMeterComponent {
   @Input() value!: string;
 
+  COLOR_CLASS_MAP: ColorClassMap = {
+    gray: 'strength-section--gray',
+    red: 'strength-section--red',
+    yellow: 'strength-section--yellow',
+    green: 'strength-section--green',
+  };
+
   get sections(): SectionColor[] {
-    return ['red', 'gray', 'gray'];
+    const hasLetters = lettersRegex.test(this.value);
+    const hasDigits = digitsRegex.test(this.value);
+    const hasSpecialChars = specialCharsRegex.test(this.value);
+    const strength =
+      Number(hasLetters) + Number(hasDigits) + Number(hasSpecialChars);
+
+    if (!this.value.length) {
+      return ['gray', 'gray', 'gray'];
+    }
+    if (this.value.length < 8) {
+      return ['red', 'red', 'red'];
+    }
+    switch (strength) {
+      case 3:
+        return ['green', 'green', 'green'];
+      case 2:
+        return ['yellow', 'yellow', 'gray'];
+      default:
+        return ['red', 'gray', 'gray'];
+    }
   }
 }
